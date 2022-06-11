43aeb05771f40aabe3ffc76ec92b14bc8418300e
Enzo <enzocass@live.com>
2022-06-11 17:19:47 -0300

    feat(design): added responsiveness in todo list

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ todoTask = document.querySelector('#task');
 todoList = document.querySelector('#todo-list');
 btn = document.querySelector('#button-submit');
 
-window.addEventListener('load', () =>{
+window.addEventListener('load', () => {
     //todos = JSON.parse(localStorage.getItem('todos')) || [];
     console.log('Loaded');
     console.log(todoTask);
@@ -17,20 +17,40 @@ window.addEventListener('load', () =>{
         }
         console.log('Submitted');
         console.log(todoTask.value);
-        todoList.innerHTML += `<div class="list--item">
-                            <div class="list--checkbox">
-                                <input type="checkbox" class="todo__list--checkbox--input">
-                            </div>
-                            <div class="list--text">
-                                <input class="list--text--title" value="${todoTask.value}" readonly />
-                            </div>
-                        <div class="list--action">
-                            <button class="list--action--edit">Edit</button>
-                            <button class="list--action--delete">Delete</button>
-                        </div>`;
-        clearInput(todoTask);
+        todoList.innerHTML += `
+        <div class="list--item">
+            <div class="list--checkbox">
+                <input type="checkbox" id="teste" class="todo__list--checkbox--input">
+            </div>
+            <div class="list--text">
+                <input class="list--text--title" value="${todoTask.value}" readonly />
+            </div>
+            <div class="list--action">
+                <button class="list--action--edit">Edit</button>
+                <button class="list--action--delete">Delete</button>
+            </div>
+        </div>`;
+        checkbox = document.querySelector('#teste');
+        console.log(checkbox);
+        console.log(checkbox.checked);
+        clearInput(todoTask.value);
     })
 })
+
+const setCheckBox = () => {
+    // evento de click no checkbox
+    // text-decoration: line-through;
+    checkbox = document.querySelector('#teste');
+    checkbox.addEventListener('click', e => {
+        console.log(e.target.checked);
+        if (e.target.checked) {
+            e.target.parentElement.parentElement.classList.add('completed');
+        } else {
+            e.target.parentElement.parentElement.classList.remove('completed');
+        }
+    }
+    )
+}
 
 const clearInput = (element) => {
     element.value = '';
